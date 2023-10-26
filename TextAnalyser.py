ba192ec595e032ab0c53e65934f3b35223769e53
denmayski <denmayski@icloud.com>
2023-10-26 17:12:36 +0300

    code refactor

---
+++ TextAnalyser.py
@@ -0,0 +1,49 @@
+from natasha import (
+    Segmenter,
+    MorphVocab,
+    NewsEmbedding,
+    NewsMorphTagger,
+    NewsNERTagger,
+    NewsSyntaxParser,
+    PER,
+    Doc
+)
+
+
+class TextAnalyser:
+    def __init__(self, text):
+        # Инициализация компонентов Natasha
+        self.segmenter = Segmenter()
+        self.morph_vocab = MorphVocab()
+        self.emb = NewsEmbedding()
+        self.morph_tagger = NewsMorphTagger(self.emb)
+        self.syntax_parser = NewsSyntaxParser(self.emb)
+        self.text = text
+
+        # Анализ текста с использованием объекта Doc
+        self.doc = Doc(self.text)
+
+        # Обработка текста Natasha
+        self.doc.segment(self.segmenter)
+        self.doc.tag_morph(self.morph_tagger)
+        self.doc.parse_syntax(self.syntax_parser)
+
+    def search_word(self, word):
+        count = 0
+        # Поиск сочетаний "слово" + прилагательное
+        for token in self.doc.tokens:
+            if word == token.text:
+                for item in self.doc.tokens:
+                    if item.head_id == token.id and item.rel == 'amod':
+                        adjective = item.text
+                        sentence_id = None
+                        for i, sentence in enumerate(self.doc.sents):
+                            if token in sentence.tokens:
+                                sentence_id = i
+                                break
+                        if sentence_id is not None:
+                            sentence_text = self.doc.sents[sentence_id].text
+                            print(f"Сочетание {token.text} {adjective}")
+                            print(f"Предложение: {sentence_text}\n")
+                            count += 1
+        return count
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import chardet
 import os
-from wordWrap import wordWrap
+from TextAnalyser import TextAnalyser
 
 while True:
     choose = input("1) Ввод текста вручную\n2) Указать ссылку на файл\n").strip()
@@ -25,26 +25,8 @@ else:
         else:
             print("Файл не найден")
 
-dd = wordWrap(text)
-
-while (wordSearch := input("Введите слово для поиска ").strip().lower()):
-    count = 0
-    # Поиск сочетаний "слово" + прилагательное
-    for token in dd.doc.tokens:
-        if wordSearch == token.text:
-            for item in dd.doc.tokens:
-                if item.head_id == token.id and item.rel == 'amod':
-                    adjective = item.text
-                    sentence_id = None
-                    for i, sentence in enumerate(dd.doc.sents):
-                        if token in sentence.tokens:
-                            sentence_id = i
-                            break
-                    if sentence_id is not None:
-                        sentence_text = dd.doc.sents[sentence_id].text
-                        print(f"Сочетание {token.text} {adjective}")
-                        print(f"Предложение: {sentence_text}\n")
-                        count += 1
-    if not count:
-        print("Это слово не встречается с прилагательными")
+dd = TextAnalyser(text)
+while wordSearch := input("Введите слово для поиска ").strip().lower():
+    if not dd.search_word(wordSearch):
+        print(f"Слово {wordSearch} не встречается с прилагательным в тексте")
     print()
