46dc407267cd20eb8bec2bf25a196787b6f2a108
denmayski <denmayski@icloud.com>
2023-10-26 14:56:11 +0300

    a1

--- main.py
+++ main.py
@@ -30,20 +30,24 @@ doc.segment(segmenter)
 doc.tag_morph(morph_tagger)
 doc.parse_syntax(syntax_parser)
 
-wordSearch = input("Введите слово для поиска ").strip().lower()
-
-# Поиск сочетаний "кошка" + прилагательное
-for token in doc.tokens:
-    if wordSearch == token.text:
-        for item in doc.tokens:
-            if item.head_id == token.id and item.rel == 'amod':
-                adjective = item.text
-                sentence_id = None
-                for i, sentence in enumerate(doc.sents):
-                    if token in sentence.tokens:
-                        sentence_id = i
-                        break
-                if sentence_id is not None:
-                    sentence_text = doc.sents[sentence_id].text
-                    print(f"Сочетание {token.text} {adjective}")
-                    print(f"Предложение: {sentence_text}\n")
+while (wordSearch := input("Введите слово для поиска ").strip().lower()):
+    count = 0
+    # Поиск сочетаний "кошка" + прилагательное
+    for token in doc.tokens:
+        if wordSearch == token.text:
+            for item in doc.tokens:
+                if item.head_id == token.id and item.rel == 'amod':
+                    adjective = item.text
+                    sentence_id = None
+                    for i, sentence in enumerate(doc.sents):
+                        if token in sentence.tokens:
+                            sentence_id = i
+                            break
+                    if sentence_id is not None:
+                        sentence_text = doc.sents[sentence_id].text
+                        print(f"Сочетание {token.text} {adjective}")
+                        print(f"Предложение: {sentence_text}\n")
+                        count+=1
+    if not count:
+        print("Это слово не встречается с прилагательными")
+    print()
